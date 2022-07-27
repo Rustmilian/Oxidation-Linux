0679e933f6553c70df1913820aba9a33f5beacb0
Rustmilian <rustmilian@proton.me>
2022-07-27 19:41:13 -0400

    seed

---
+++ profiledef.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="OxidationLinux"
+iso_label="ARCH_$(date +%Y%m)"
+iso_publisher="Rustmilian <https://github.com/Rustmilian/Oxidation-Linux>"
+iso_application="Oxidation Linux Live CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr'
+           'bios.syslinux.eltorito'
+	   'uefi-ia32.grub.esp'
+           'uefi-ia32.grub.eltorito'
+           'uefi-x64.systemd-boot.esp'
+           'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
+encryption_key="auto"
+keys_image_type="erofs"
+keys_image_tool_options=('-zlz4hc,12')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/run/archiso/keys"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/setup-persistent-storage"]="0:0:755"
+)
