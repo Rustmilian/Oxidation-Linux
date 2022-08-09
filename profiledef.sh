b65bd3f924d649cdfb4af88195911def190aafaa
Rustmilian <rustmilian@proton.me>
2022-08-08 21:19:40 -0400

    update

--- profiledef.sh
+++ profiledef.sh
@@ -1,33 +1,25 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="OxidationLinux"
+iso_name="oxidationlinux"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Rustmilian <https://github.com/Rustmilian/Oxidation-Linux>"
-iso_application="Oxidation Linux Live CD"
+iso_application="Oxidation Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr'
-           'bios.syslinux.eltorito'
-	   'uefi-ia32.grub.esp'
-           'uefi-ia32.grub.eltorito'
-           'uefi-x64.systemd-boot.esp'
-           'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
-encryption_key="auto"
-keys_image_type="erofs"
-keys_image_tool_options=('-zlz4hc,12')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/run/archiso/keys"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/setup-persistent-storage"]="0:0:755"
 )
